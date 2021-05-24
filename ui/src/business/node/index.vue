494fbbc542a38a60f90a02c2db05a0638deadbfb
chenyang <907693148@qq.com>
2021-05-24 15:51:27 +0800

    feat(proxy): 增加namespace proxy api

--- ui/src/business/node/index.vue
+++ ui/src/business/node/index.vue
@@ -1,14 +1,22 @@
 <template>
-  <layout-content></layout-content>
+    <layout-content></layout-content>
 </template>
 
 <script>
-import LayoutContent from "@/components/layout/LayoutContent"
+    import LayoutContent from "@/components/layout/LayoutContent"
+    import {listNamespace} from "@/api/namespace"
 
-export default {
-  name: "NodeList",
-  components: { LayoutContent }
-}
+    export default {
+        name: "NodeList",
+        components: {LayoutContent},
+        methods: {},
+        mounted() {
+            listNamespace().then(data => {
+                console.log(data)
+            })
+
+        }
+    }
 </script>
 
 <style scoped>
