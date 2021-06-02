3f12b4e7b6a94f1d24a60a3ec92fbe9d16a682f2
wangzhengkun <zhengkun@fit2cloud.com>
2021-06-02 16:10:06 +0800

    feat(style): 修改样式

---
+++ ui/src/business/app-layout/header-components/UserSetting.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-dropdown  trigger="click">
+    <span class="el-dropdown-link">
+        <i class="el-icon-user-solid" style="margin-right: 3px"></i>
+        <span>用户</span>
+        <i class="el-icon-arrow-down el-icon--right"></i>
+    </span>
+  </el-dropdown>
+</template>
+
+<script>
+export default {
+  name: "UserSetting"
+}
+</script>
+
+<style scoped>
+
+</style>
