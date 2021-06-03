2849b7e29d60e2f79bb9a16fcf7674adc90af1f3
chenyang <907693148@qq.com>
2021-06-03 15:20:43 +0800

    feat(login): 登陆实现

--- ui/src/permission.js
+++ ui/src/permission.js
@@ -4,62 +4,63 @@ import "nprogress/nprogress.css"
 import store from "./store"
 import Layout from "@/business/app-layout/horizontal-layout"
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
+NProgress.configure({showSpinner: false}) // NProgress Configuration
 
 const whiteList = ["/login"] // no redirect whitelist
 
 const generateRoutes = async (to, from, next) => {
-  const hasRoles = store.getters.roles && store.getters.roles.length > 0
-  if (hasRoles) {
-    next()
-  } else {
-    try {
-      const user = await store.dispatch("user/getCurrentUser")
-      const accessRoutes = await store.dispatch("permission/generateRoutes", user)
-      if (user.menu === 'global'){
-        router.addRoutes([  {
-          path: "/",
-          component: Layout,
-          redirect: "/clusterlist",
-        },])
-      }else{
-        router.addRoutes([  {
-          path: "/",
-          component: Layout,
-          redirect: "/dashboard",
-        },])
-      }
-      router.addRoutes(accessRoutes)
-      next({ ...to, replace: true })
-    } catch (error) {
-      NProgress.done()
+    const hasRoles = store.getters.roles && store.getters.roles.length > 0
+    if (hasRoles) {
+        next()
+    } else {
+        try {
+            const user = await store.dispatch("user/getCurrentUser")
+            user.menu = 'global'
+            const accessRoutes = await store.dispatch("permission/generateRoutes", user)
+            if (user.menu === 'global') {
+                router.addRoutes([{
+                    path: "/",
+                    component: Layout,
+                    redirect: "/clusterlist",
+                },])
+            } else {
+                router.addRoutes([{
+                    path: "/",
+                    component: Layout,
+                    redirect: "/dashboard",
+                },])
+            }
+            router.addRoutes(accessRoutes)
+            next({...to, replace: true})
+        } catch (error) {
+            NProgress.done()
+        }
     }
-  }
 }
 
 //路由前置钩子，根据实际需求修改
 router.beforeEach(async (to, from, next) => {
-  NProgress.start()
-  const isLogin = await store.dispatch("user/isLogin")
-  if (isLogin) {
-    if (to.path === "/login") {
-      next({ path: "/" })
-      NProgress.done()
-    }
-    await generateRoutes(to, from, next)
-  } else {
-    /* has not login*/
-    if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
-      next()
+    NProgress.start()
+    const isLogin = await store.dispatch("user/isLogin")
+    if (isLogin) {
+        if (to.path === "/login") {
+            next({path: "/"})
+            NProgress.done()
+        }
+        await generateRoutes(to, from, next)
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
+        /* has not login*/
+        if (whiteList.indexOf(to.path) !== -1) {
+            // in the free login whitelist, go directly
+            next()
+        } else {
+            // other pages that do not have permission to access are redirected to the login page.
+            next(`/login?redirect=${to.path}`)
+            NProgress.done()
+        }
     }
-  }
 })
 
 router.afterEach(() => {
-  NProgress.done()
+    NProgress.done()
 })
--- ui/src/store/modules/user.js
+++ ui/src/store/modules/user.js
@@ -1,108 +1,99 @@
-/* 前后端不分离的登录方式*/
-// import {updateUser} from "@/api/user"
-// import {login, logout, isLogin, getSession} from "@/api/auth"
-// import {resetRouter} from "@/router"
+import {login, isLogin, logout, getCurrentUser} from "@/api/auth"
+import {resetRouter} from "@/router"
 import {getLanguage, setLanguage} from "@/i18n"
 
 const state = {
-  login: false,
-  name: "",
-  currentProject: "",
-  language: getLanguage(),
-  roles: []
+    login: false,
+    name: "",
+    currentProject: "",
+    language: getLanguage(),
+    roles: []
 }
 
 const mutations = {
-  LOGIN: (state) => {
-    state.login = true
-  },
-  LOGOUT: (state) => {
-    state.login = false
-  },
-  SET_NAME: (state, name) => {
-    state.name = name
-  },
-  SET_LANGUAGE: (state, language) => {
-    state.language = language
-    setLanguage(language)
-  },
-  SET_ROLES: (state, roles) => {
-    state.roles = roles
-  },
-  SET_CURRENT_MENU: (state, menu) => {
-    state.menu = menu
-  },
+    LOGIN: (state) => {
+        state.login = true
+    },
+    LOGOUT: (state) => {
+        state.login = false
+    },
+    SET_NAME: (state, name) => {
+        state.name = name
+    },
+    SET_LANGUAGE: (state, language) => {
+        state.language = language
+        setLanguage(language)
+    },
+    SET_ROLES: (state, roles) => {
+        state.roles = roles
+    },
+    SET_CURRENT_MENU: (state, menu) => {
+        state.menu = menu
+    },
 }
 
 const actions = {
-  login ({ commit }, userInfo) {
-    console.log(userInfo)
-    // const { username, password, captchaId, code } = userInfo
-    return new Promise((resolve) => {
-      commit("LOGIN")
-      const user = {
-        name: "admin",
-        roles: ['ADMIN'],
-        menu: "global"
-      }
-      resolve(user)
-      // login({ username: username.trim(), password: password, captchaId: captchaId, code: code }).then(response => {
-      //   commit("LOGIN")
-      //   resolve(response)
-      // }).catch(error => {
-      //   reject(error)
-      // })
-    })
-  },
+    login({commit}, userInfo) {
+        const {username, password} = userInfo
+        return new Promise((resolve, reject) => {
+            commit("LOGIN")
+            login({username: username.trim(), password: password}).then(response => {
+                commit("LOGIN")
+                resolve(response)
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    },
 
-  isLogin () {
-    return new Promise((resolve) => {
-      if (state.login) {
-        resolve(true)
-      }else {
-        resolve(false)
-      }
-      // isLogin().then((data) => {
-      //   if (data.isLogin) {
-      //     commit("LOGIN")
-      //     resolve(true)
-      //   } else {
-      //     resolve(false)
-      //   }
-      // }).catch(() => {
-      //   resolve(false)
-      // })
-    })
-  },
+    isLogin(commit) {
+        return new Promise((resolve) => {
+            if (state.login) {
+                resolve(true)
+            }
+            isLogin().then((data) => {
+                if (data.data) {
+                    commit("LOGIN")
+                    resolve(true)
+                } else {
+                    resolve(false)
+                }
+            }).catch(() => {
+                resolve(false)
+            })
+        })
+    },
 
-  getCurrentUser ({ commit }) {
-    return new Promise((resolve) => {
-      const user = {
-        name: "admin",
-        roles: ['ADMIN'],
-        menu: "global"
-      }
-      commit("SET_CURRENT_MENU", user.menu)
-      commit("SET_ROLES", user.roles)
-      resolve(user)
-      // getSession().then(data => {
-      //   const user = data.user
-      //   const { name, roles, language, currentProject } = user
-      //   commit("SET_NAME", name)
-      //   commit("SET_ROLES", roles)
-      //   commit("SET_LANGUAGE", language)
-      //   commit("SET_CURRENT_PROJECT", currentProject)
-      //   resolve(user)
-      // }).catch(error => {
-      //   reject(error)
-      // })
-    })
-  },
+    getCurrentUser({commit}) {
+        return new Promise((resolve, reject) => {
+            getCurrentUser().then(data => {
+
+                const user = data.data
+                user["roles"] = ["ADMIN"]
+                user["menu"] = ["global"]
+                const {name, roles, menu} = user
+                commit("SET_NAME", name)
+                commit("SET_ROLES", roles)
+                commit("SET_LANGUAGE", "zh-CN")
+                commit("SET_CURRENT_MENU", menu)
+                resolve(user)
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    },
+    logout({commit}) {
+        logout().then(() => {
+            commit('LOGOUT')
+            commit('SET_ROLES', [])
+            resetRouter()
+        })
+    },
 }
 
 export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions
+    namespaced: true,
+    state,
+    mutations,
+    actions
 }
