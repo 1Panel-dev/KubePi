93f7616d602a91912417025665d333d3ea4bdd67
wangzhengkun <zhengkun@fit2cloud.com>
2021-07-14 18:34:30 +0800

    feat(filter): 样式修改

--- web/dashboard/src/filters/index.js
+++ web/dashboard/src/filters/index.js
@@ -9,19 +9,19 @@ export function AgeFormat (value) {
   let dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000)); //计算出相差天数
   let result = ""
   if (dayDiff > 0 ) {
-    result = result + dayDiff + "days ago"
+    result = result + dayDiff + " days ago"
     return  result
   }
   let leave1 = dateDiff % (24 * 3600 * 1000); //计算天数后剩余的毫秒数
   let hours = Math.floor(leave1 / (3600 * 1000)); //计算出小时数
   if (hours > 0 ) {
-    result = result + dayDiff + "hours ago"
+    result = result + dayDiff + " hours ago"
     return  result
   }
   let leave2 = leave1 % (3600 * 1000); //计算小时数后剩余的毫秒数
   let minutes = Math.floor(leave2 / (60 * 1000)); //计算相差分钟
   if (minutes> 0 ) {
-    result = result + dayDiff + "minutes ago"
+    result = result + dayDiff + " minutes ago"
   }
   return result
 }
