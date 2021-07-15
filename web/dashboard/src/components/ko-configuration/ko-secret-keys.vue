5e4685a5798ac0b64bda69f13839b2ecffd97ff2
wangzhengkun <zhengkun@fit2cloud.com>
2021-07-15 15:53:38 +0800

    feat(secret): 增加校验

--- web/dashboard/src/components/ko-configuration/ko-secret-keys.vue
+++ web/dashboard/src/components/ko-configuration/ko-secret-keys.vue
@@ -1,16 +1,16 @@
 <template>
   <div style="margin-top: 20px">
     <ko-card title="Keys">
-      <el-form label-position="top" ref="form" :model="form">
+      <el-form label-position="top" ref="form" :model="form" :rules="rules">
         <el-row :gutter="20">
           <el-col :span="12">
             <el-form-item label="Public Key" prop="publicKey" required>
-              <ko-form-item itemType="textarea"  v-model="form.publicKey" @change.native="transform"></ko-form-item>
+              <ko-form-item itemType="textarea" v-model="form.publicKey" @change.native="transform"></ko-form-item>
             </el-form-item>
           </el-col>
           <el-col :span="12">
             <el-form-item label="Private Key" prop="privateKey" required>
-              <ko-form-item itemType="textarea"  v-model="form.privateKey" @change.native="transform"></ko-form-item>
+              <ko-form-item itemType="textarea" v-model="form.privateKey" @change.native="transform"></ko-form-item>
             </el-form-item>
           </el-col>
         </el-row>
@@ -22,6 +22,7 @@
 <script>
 import KoCard from "@/components/ko-card"
 import KoFormItem from "@/components/ko-form-item"
+import Rule from "@/utils/rules"
 
 export default {
   name: "KoSecretKeys",
@@ -34,10 +35,23 @@ export default {
       form: {
         privateKey: "",
         publicKey: ""
+      },
+      rules: {
+        publicKey: [Rule.RequiredRule]
       }
     }
   },
   methods: {
+    checkIsValid () {
+      let result = {
+        success: false,
+        messages: ["测试用户"]
+      }
+      this.$refs["form"].validate((valid) => {
+        result.success = valid
+      })
+      return result
+    },
     transform () {
       const { Base64 } = require("js-base64")
       const result = {
@@ -48,11 +62,13 @@ export default {
     }
   },
   mounted () {
-    if (this.dataObj) {
+    if (this.dataObj && this.dataObj !== {}) {
       const { Base64 } = require("js-base64")
-      this.form = {
-        privateKey: Base64.decode(this.dataObj["ssh-privatekey"]),
-        publicKey: Base64.decode(this.dataObj["ssh-publickey"])
+      if (this.dataObj["ssh-privatekey"]) {
+        this.form.privateKey = Base64.decode(this.dataObj["ssh-privatekey"])
+      }
+      if (this.dataObj["ssh-publickey"]) {
+        this.form.publicKey = Base64.decode(this.dataObj["ssh-publickey"])
       }
     }
   }
