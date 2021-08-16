6f94d2ad00350a77cf359a4bc45d8999f69d893e
wangzhengkun <zhengkun@fit2cloud.com>
2021-08-16 16:02:25 +0800

    style(ekko): 修改登录页样式

--- web/ekko/src/business/login/index.vue
+++ web/ekko/src/business/login/index.vue
@@ -1,255 +1,264 @@
 <template xmlns:el-col="http://www.w3.org/1999/html">
-    <div class="login-background">
-        <div class="login-container">
-            <el-row type="flex" v-loading="loading">
-                <el-col :span="12">
-                    <el-form :model="form" :rules="rules" ref="form" size="default">
-                        <div class="login-title">
-                            {{ systemName }}
-                        </div>
-                        <div class="login-border"></div>
-                        <div class="login-welcome">
-                            {{ $t("commons.login.welcome") }}
-                        </div>
-                        <div class="login-form">
-                            <el-form-item prop="username">
-                                <el-input v-model="form.username" :placeholder="$t('commons.login.username_or_email')" autofocus/>
-                            </el-form-item>
-                            <el-form-item prop="password">
-                                <el-input type="password" v-model="form.password" :placeholder="$t('commons.login.password')"
-                                          maxlength="30"/>
-                            </el-form-item>
-                        </div>
-                        <div class="login-btn">
-                            <el-button type="primary" class="submit" @click="submit('form')" size="default">
-                                {{ $t("commons.button.login") }}
-                            </el-button>
-                        </div>
-                    </el-form>
-                </el-col>
-                <el-col :span="12">
-                    <div class="login-image"></div>
-                </el-col>
-            </el-row>
-        </div>
+  <div class="login-background">
+    <div class="login-container">
+      <el-row type="flex" v-loading="loading">
+        <el-col :span="12">
+          <el-form :model="form" :rules="rules" ref="form" size="default">
+            <div class="login-title">
+              {{ systemName }}
+            </div>
+            <div class="login-border"></div>
+            <div class="login-welcome">
+              {{ $t("commons.login.welcome") }}
+            </div>
+            <div class="login-form">
+              <el-form-item prop="username" class="login">
+                <el-input v-model="form.username" :placeholder="$t('commons.login.username_or_email')"
+                          autofocus/>
+              </el-form-item>
+              <el-form-item prop="password" class="login">
+                <el-input type="password" v-model="form.password" :placeholder="$t('commons.login.password')"
+                          maxlength="30"/>
+              </el-form-item>
+            </div>
+            <div class="login-btn">
+              <el-button type="primary" class="submit" @click="submit('form')" size="default">
+                {{ $t("commons.button.login") }}
+              </el-button>
+            </div>
+          </el-form>
+        </el-col>
+        <el-col :span="12">
+          <div class="login-image"></div>
+        </el-col>
+      </el-row>
     </div>
+  </div>
 </template>
 <script>
 
 
-    export default {
-        name: "Login",
-        data() {
-            return {
-                loading: false,
-                form: {
-                    username: "",
-                    password: "",
-                },
-                rules: {
-                    username: [
-                        {
-                            required: true,
-                            message: this.$tm("commons.validate.input", "commons.login.username_or_email"),
-                            trigger: "blur"
-                        },
-                    ],
-                    password: [
-                        // 先去掉方便测试
-                        {
-                            required: true,
-                            message: this.$tm("commons.validate.input", "commons.login.password"),
-                            trigger: "blur"
-                        },
-                    ],
-                },
-                msg: "",
-                redirect: undefined,
-                otherQuery: {},
-                systemName: this.$t("commons.login.title"),
-                loadingPage: false
-            }
-        },
-        watch: {
-            $route: {
-                handler: function (route) {
-                    const query = route.query
-                    if (query) {
-                        this.redirect = query.redirect
-                        this.otherQuery = this.getOtherQuery(query)
-                    }
-                },
-                immediate: true
-            }
-        },
-        created: function () {
-            document.addEventListener("keydown", this.watchEnter)
-        },
-
-        destroyed() {
-            document.removeEventListener("keydown", this.watchEnter)
-        },
-        methods: {
-            watchEnter(e) {
-                let keyCode = e.keyCode
-                if (keyCode === 13) {
-                    this.submit("form")
-                }            },
-            submit(form) {
-                this.$refs[form].validate((valid) => {
-                    if (valid) {
-                        this.loading = true
-                        this.$store.dispatch("user/login", this.form).then(() => {
-                            this.$router.push({path: this.redirect || "/", query: this.otherQuery})
-                            this.loading = false
-                        }).catch(() => {
-                            this.loading = false
-                        })
-                    } else {
-                        return false
-                    }
-                })
-            },
-            getOtherQuery(query) {
-                return Object.keys(query).reduce((acc, cur) => {
-                    if (cur !== "redirect") {
-                        acc[cur] = query[cur]
-                    }
-                    return acc
-                }, {})
-            },
-        },
+export default {
+  name: "Login",
+  data () {
+    return {
+      loading: false,
+      form: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: this.$tm("commons.validate.input", "commons.login.username_or_email"),
+            trigger: "blur"
+          },
+        ],
+        password: [
+          // 先去掉方便测试
+          {
+            required: true,
+            message: this.$tm("commons.validate.input", "commons.login.password"),
+            trigger: "blur"
+          },
+        ],
+      },
+      msg: "",
+      redirect: undefined,
+      otherQuery: {},
+      systemName: this.$t("commons.login.title"),
+      loadingPage: false
     }
+  },
+  watch: {
+    $route: {
+      handler: function (route) {
+        const query = route.query
+        if (query) {
+          this.redirect = query.redirect
+          this.otherQuery = this.getOtherQuery(query)
+        }
+      },
+      immediate: true
+    }
+  },
+  created: function () {
+    document.addEventListener("keydown", this.watchEnter)
+  },
+
+  destroyed () {
+    document.removeEventListener("keydown", this.watchEnter)
+  },
+  methods: {
+    watchEnter (e) {
+      let keyCode = e.keyCode
+      if (keyCode === 13) {
+        this.submit("form")
+      }
+    },
+    submit (form) {
+      this.$refs[form].validate((valid) => {
+        if (valid) {
+          this.loading = true
+          this.$store.dispatch("user/login", this.form).then(() => {
+            this.$router.push({ path: this.redirect || "/", query: this.otherQuery })
+            this.loading = false
+          }).catch(() => {
+            this.loading = false
+          })
+        } else {
+          return false
+        }
+      })
+    },
+    getOtherQuery (query) {
+      return Object.keys(query).reduce((acc, cur) => {
+        if (cur !== "redirect") {
+          acc[cur] = query[cur]
+        }
+        return acc
+      }, {})
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-    @import "../../styles/common/variables";
+  @import "../../styles/common/variables";
 
-    @mixin login-center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+  @mixin login-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-background {
+    background-color: #FFFFFF;
+    height: 100%;
+    @include login-center;
+  }
+
+  .login-container {
+    min-width: 640px;
+    width: 960px;
+    height: 480px;
+    background-color: #FFFFFF;
+    box-shadow: 0 5px 5px -3px rgba(0, 0, 0, .2), 0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12);
+    @media only screen and (max-width: 1280px) {
+      width: 900px;
+      height: 380px;
     }
 
-    .login-background {
-        background-color: #FFFFFF;
-        height: 100%;
-        @include login-center;
+    .login-logo {
+      margin-top: 30px;
+      margin-left: 30px;
+      @media only screen and (max-width: 1280px) {
+        margin-top: 20px;
+      }
+
+      img {
+        height: 45px;
+      }
     }
 
-    .login-container {
-        min-width: 640px;
-        width: 960px;
-        height: 480px;
-        background-color: #FFFFFF;
-        box-shadow: 0 5px 5px -3px rgba(0, 0, 0, .2), 0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12);
-        @media only screen and (max-width: 1280px) {
-            width: 900px;
-            height: 380px;
-        }
+    .login-title {
+      margin-top: 50px;
+      font-size: 32px;
+      letter-spacing: 0;
+      text-align: center;
+      color: #999999;
 
-        .login-logo {
-            margin-top: 30px;
-            margin-left: 30px;
-            @media only screen and (max-width: 1280px) {
-                margin-top: 20px;
-            }
-
-            img {
-                height: 45px;
-            }
-        }
-
-        .login-title {
-            margin-top: 50px;
-            font-size: 32px;
-            letter-spacing: 0;
-            text-align: center;
-            color: #999999;
-
-            @media only screen and (max-width: 1280px) {
-                margin-top: 20px;
-            }
-        }
-
-        .login-border {
-            height: 2px;
-            margin: 20px auto 20px;
-            position: relative;
-            width: 80px;
-            background: $--color-primary;
-            @media only screen and (max-width: 1280px) {
-                margin: 10px auto 10px;
-            }
-        }
-
-        .login-welcome {
-            margin-top: 10px;
-            font-size: 14px;
-            color: #999999;
-            letter-spacing: 0;
-            line-height: 18px;
-            text-align: center;
-            @media only screen and (max-width: 1280px) {
-                margin-top: 20px;
-            }
-        }
-
-        .login-form {
-            margin-top: 30px;
-            padding: 0 40px;
-
-            @media only screen and (max-width: 1280px) {
-                margin-top: 10px;
-            }
-
-            & ::v-deep .el-input__inner {
-                border-radius: 0;
-            }
-        }
-
-        .login-btn {
-            margin-top: 40px;
-            padding: 0 40px;
-            @media only screen and (max-width: 1280px) {
-                margin-top: 20px;
-            }
-
-            .submit {
-                width: 100%;
-                border-radius: 0;
-            }
-        }
-
-        .login-msg {
-            margin-top: 10px;
-            padding: 0 40px;
-            color: $--color-danger;
-            text-align: center;
-        }
-
-        .login-image {
-            background: url(../../assets/KobeOperator-login.jpg) no-repeat;
-            background-size: cover;
-            width: 100%;
-            height: 480px;
-            @media only screen and (max-width: 1280px) {
-                height: 380px;
-            }
-        }
-
-        .submit {
-            width: 100%;
-            border-radius: 0;
-        }
-
-        .forget-password {
-            margin-top: 40px;
-            padding: 0 40px;
-            float: right;
-            @media only screen and (max-width: 1280px) {
-                margin-top: 20px;
-            }
-        }
+      @media only screen and (max-width: 1280px) {
+        margin-top: 20px;
+      }
     }
+
+    .login-border {
+      height: 2px;
+      margin: 20px auto 20px;
+      position: relative;
+      width: 80px;
+      background: $--color-primary;
+      @media only screen and (max-width: 1280px) {
+        margin: 10px auto 10px;
+      }
+    }
+
+    .login-welcome {
+      margin-top: 10px;
+      font-size: 14px;
+      color: #999999;
+      letter-spacing: 0;
+      line-height: 18px;
+      text-align: center;
+      @media only screen and (max-width: 1280px) {
+        margin-top: 20px;
+      }
+    }
+
+    .login-form {
+      margin-top: 30px;
+      padding: 0 40px;
+
+      @media only screen and (max-width: 1280px) {
+        margin-top: 10px;
+      }
+
+      & ::v-deep .el-input__inner {
+        border-radius: 0;
+      }
+    }
+
+    .login-btn {
+      margin-top: 40px;
+      padding: 0 40px;
+      @media only screen and (max-width: 1280px) {
+        margin-top: 20px;
+      }
+
+      .submit {
+        width: 100%;
+        border-radius: 0;
+      }
+    }
+
+    .login-msg {
+      margin-top: 10px;
+      padding: 0 40px;
+      color: $--color-danger;
+      text-align: center;
+    }
+
+    .login-image {
+      background: url(../../assets/KobeOperator-login.jpg) no-repeat;
+      background-size: cover;
+      width: 100%;
+      height: 480px;
+      @media only screen and (max-width: 1280px) {
+        height: 380px;
+      }
+    }
+
+    .submit {
+      width: 100%;
+      border-radius: 0;
+    }
+
+    .forget-password {
+      margin-top: 40px;
+      padding: 0 40px;
+      float: right;
+      @media only screen and (max-width: 1280px) {
+        margin-top: 20px;
+      }
+    }
+
+  }
+
+  .login /deep/ input {
+    background-color: #FFFFFF !important;
+    color: #000000 !important;
+
+  }
 </style>
