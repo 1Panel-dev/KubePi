cdca82715329a84e03f12e8a20d5f285ec3b1a6b
Aaron3S <34506289+Aaron3S@users.noreply.github.com>
2021-08-24 19:14:35 +0800

    fix(i18n): 修改翻译

--- pkg/i18n/cn_zh.go
+++ pkg/i18n/cn_zh.go
@@ -1,6 +1,6 @@
 package i18n
 
 var zhCNMapping = TextMapping{
-	"already exists":             "资源已存在,请尝尝试修改资源名称",
+	"already exists":             "资源已存在,请尝试修改资源名称",
 	"username or password error": "登录失败,用户名或密码错误",
 }
