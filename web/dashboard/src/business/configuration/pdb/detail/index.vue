ceb433cb5d07bb482e2961aee4acff5abbb5538c
wangzhengkun <zhengkun@fit2cloud.com>
2021-08-26 15:03:37 +0800

    feat(detail): 部分页面增加返回按钮

--- web/dashboard/src/business/configuration/pdb/detail/index.vue
+++ web/dashboard/src/business/configuration/pdb/detail/index.vue
@@ -1,6 +1,9 @@
 <template>
   <layout-content :header="$t('commons.form.detail')" :back-to="{name: 'PDBs'}"  v-loading="loading">
     <yaml-editor :value="yaml" :read-only="true"></yaml-editor>
+    <div class="bottom-button">
+      <el-button @click="onCancel()">{{ $t("commons.button.back_detail") }}</el-button>
+    </div>
   </layout-content>
 </template>
 
@@ -31,6 +34,9 @@ export default {
         this.yaml = JSON.parse(JSON.stringify(this.item))
         this.loading = false
       })
+    },
+    onCancel() {
+      this.$router.push({ name: "PDBs" })
     }
   },
   created () {
