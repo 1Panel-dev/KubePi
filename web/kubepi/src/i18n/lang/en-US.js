4bd4b0b652c5b7dbbdf2f6a98428255f93959862
ssongliu <songlius11@163.com>
2021-10-26 14:32:50 +0800

    feat(kubepi): 增加系统关于信息

--- web/kubepi/src/i18n/lang/en-US.js
+++ web/kubepi/src/i18n/lang/en-US.js
@@ -10,7 +10,20 @@ const message = {
     },
     personal: {
       profile: "Profile",
-      exit: "Logout"
+      exit: "Logout",
+      project_url: "Project Address",
+      issue: "Feedback",
+      talk: "Participate in the discussion",
+      star: "Star",
+      version: "Version",
+      introduction: "KubePi is an easy to use open source Kubernetes visual management panel.",
+      introduction2: "KubePi allows administrators to import multiple Kubernetes clusters and assign permissions of different clusters and namespaces to specific users through permission control. It allows developers to manage and troubleshoot applications running in the Kubernetes cluster to better deal with the complexity in the Kubernetes cluster."
+    },
+    help: {
+      help: "Help",
+      about: "About",
+      business_support: "Business Support",
+      ko_docs: "Official Document",
     },
     button: {
       detail: "Detail",
--- web/kubepi/src/i18n/lang/zh-CN.js
+++ web/kubepi/src/i18n/lang/zh-CN.js
@@ -11,7 +11,20 @@ const message = {
     },
     personal: {
       profile: "个人信息",
-      exit: "退出"
+      exit: "退出",
+      project_url: "项目地址",
+      issue: "问题反馈",
+      talk: "参与讨论",
+      star: "点亮 Star",
+      version: "版本",
+      introduction: "KubePi 是一款简单易用的开源 Kubernetes 可视化管理面板。",
+      introduction2: "KubePi 允许管理员导入多个 Kubernetes 集群，并且通过权限控制，将不同 cluster、namespace 的权限分配给指定用户。它允许开发人员管理 Kubernetes 集群中运行的应用程序并对其进行故障排查，供开发人员更好地处理 Kubernetes 集群中的复杂性。",
+    },
+    help: {
+      help: "帮助",
+      about: "关于",
+      business_support: "商业支持",
+      ko_docs: "官方文档",
     },
     button: {
       detail: "详情",
