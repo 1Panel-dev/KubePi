6593f5b9df2a9b1c079a78b16ee6bc402044c6a4
ssongliu <songlius11@163.com>
2021-12-01 17:04:43 +0800

    fix(terminal): 终端日志下载增加时间限制

--- web/dashboard/src/business/workloads/terminal/index.vue
+++ web/dashboard/src/business/workloads/terminal/index.vue
@@ -176,6 +176,9 @@ export default {
             },
           },
         ],
+        disabledDate: (time) => {
+          return time.getTime() > Date.now()
+        },
       },
     }
   },
@@ -212,7 +215,7 @@ export default {
       params.timestamps = true
       console.log(this.form.limitDate)
       if (this.form.limitDate) {
-        params.sinceSeconds = (new Date().getTime() - this.form.limitDate.getTime())
+        params.sinceSeconds = new Date().getTime() - this.form.limitDate.getTime()
       }
       if (this.form.limitBytes) {
         params.limitBytes = this.form.limitBytes * 1024 * 1024
