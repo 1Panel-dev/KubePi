7fc27a75385c87bfdde742660cfaff59d096b52f
wangzhengkun <zhengkun@fit2cloud.com>
2022-05-11 16:29:10 +0800

    feat: 增加文件夹路由导航

--- web/dashboard/src/business/workloads/pods/podfilebrowser/index.vue
+++ web/dashboard/src/business/workloads/pods/podfilebrowser/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <layout-content :header="name">
+  <layout-content :header="name" :back-to="{name: 'Pods'}">
+    <div>
+      <i class="el-icon-folder-opened" @click="openRoot"></i>
+      <span v-for="(v,i) in folders" :key="i" >
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <span>/</span>
+         &nbsp;&nbsp;&nbsp;&nbsp;
+        <el-link class="primary" :disabled="i === (folders.length -1 )" @click="linkTo(v)"> {{v}}</el-link>
+      </span>
+    </div>
     <complex-table :selects.="selects" :data="data" v-loading="loading">
       <el-table-column type="selection" fix></el-table-column>
       <el-table-column :label="$t('commons.table.name')" prop="name" min-width="80" show-overflow-tooltip fix>
         <template v-slot:default="{row}">
-          <span v-if="row.isDir"  class="span-link" @click="changeFolder(row.name)"><i class="el-icon-folder"></i> {{row.name}} </span>
+          <span v-if="row.isDir"  class="span-link" @click="toFolder(row.name)"><i class="el-icon-folder"></i> {{row.name}} </span>
           <span v-else>{{row.name}}</span>
         </template>
       </el-table-column>
@@ -38,20 +47,38 @@ export default {
       selects: [],
       data:[],
       loading: false,
-      folder: "/"
+      folder: "/",
+      folders: []
     }
   },
   methods: {
-    changeFolder(folder) {
-      this.folder += folder
-      this.fileRequest.path = this.folder
-      this.listFiles()
+    openRoot() {
+      this.listFiles("/",[])
     },
-
-    listFiles() {
+    linkTo(folder) {
+      let folderDir = "/"
+      let folders = []
+      for(const v of this.folders) {
+        folders.push(v)
+        folderDir = folderDir + v + "/"
+        if (folder === v) {
+          break
+        }
+      }
+      this.listFiles(folderDir,folders)
+    },
+    toFolder(folder) {
+      const folderDir = this.folder + folder + "/"
+      const newFolders = this.folders.concat([folder])
+      this.listFiles(folderDir,newFolders)
+    },
+    listFiles(folder,folders) {
       this.loading = true
+      this.fileRequest.path = folder
       listPodFiles(this.fileRequest).then(res => {
         this.data = res.data
+        this.folder = folder
+        this.folders = folders
       }).finally(() => {
         this.loading = false
       })
@@ -62,10 +89,9 @@ export default {
       cluster: this.$route.query.cluster,
       namespace: this.namespace,
       podName: this.name,
-      containerName: this.$route.query.container,
-      path: this.folder
+      containerName: this.$route.query.container
     }
-    this.listFiles()
+    this.listFiles("/",[])
   }
 }
 </script>
