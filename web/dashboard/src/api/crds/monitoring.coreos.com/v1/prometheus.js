a1e7a2bfe4bdcaecf181adef9dbfb4a7270fee4b
awol2005ex <awol2005ex@163.com>
2024-07-03 16:49:59 +0800

    新增功能:增加POD容器的cpu，mem，fs监控历史图表 (#229)
    feat: 增加 POD 容器的cpu，mem，fs监控历史图表

---
+++ web/dashboard/src/api/crds/monitoring.coreos.com/v1/prometheus.js
@@ -0,0 +1,41 @@
+import {get} from "@/plugins/request"
+import qs from "qs";
+const prometheusUrl = (cluster_name) => {
+  return `/api/v1/proxy/${cluster_name}/k8s/apis/monitoring.coreos.com/v1/prometheuses`
+}
+const prometheusMetricsUrl = (cluster_name,namespace,prometheus_service_name,prometheus_prefix) => {
+  return `/api/v1/proxy/${cluster_name}/k8s/api/v1/namespaces/${namespace}/services/${prometheus_service_name}:80/proxy${prometheus_prefix}api/v1/query_range`
+}
+/*获取已部署的prometheus服务器*/
+export function listPrometheuses (cluster_name) {
+    let url = prometheusUrl(cluster_name)
+    const param = {}
+    return get(url, param)
+}
+
+/*获取容器指标（mem and fs）*/
+export function getContainerMetricsMemAndFs (cluster_name,namespace,prometheus_service_name,prometheus_prefix,pod_namespace,pod_name,container_name,secs) {
+  let url = prometheusMetricsUrl(cluster_name,namespace,prometheus_service_name,prometheus_prefix)
+  const startTimestamp = Math.floor(Date.now() / 1000) - secs; // Unix timestamp for 30 minutes ago
+  const endTimestamp = Math.floor(Date.now() / 1000); // Current Unix timestamp
+  const param = {
+    "query": `{__name__=~"container_fs_usage_bytes|container_memory_working_set_bytes",namespace="${pod_namespace}",pod="${pod_name}",container="${container_name}"}`,
+    "start": startTimestamp,
+    "end": endTimestamp,
+    "step": "1m"
+  }
+  return get(url+"?"+qs.stringify(param) , {})
+}
+/*获取容器指标（mem and fs）*/
+export function getContainerMetricsCpu(cluster_name,namespace,prometheus_service_name,prometheus_prefix,pod_namespace,pod_name,container_name,secs) {
+  let url = prometheusMetricsUrl(cluster_name,namespace,prometheus_service_name,prometheus_prefix)
+  const startTimestamp = Math.floor(Date.now() / 1000) - secs; // Unix timestamp for 30 minutes ago
+  const endTimestamp = Math.floor(Date.now() / 1000); // Current Unix timestamp
+  const param = {
+    "query": `sum(irate(container_cpu_usage_seconds_total{namespace="${pod_namespace}",pod="${pod_name}",container="${container_name}"}[5m]))`,
+    "start": startTimestamp,
+    "end": endTimestamp,
+    "step": "1m"
+  }
+  return get(url+"?"+qs.stringify(param) , {})
+}
---
+++ web/dashboard/src/components/detail/detail-containers-metrics.vue
@@ -0,0 +1,409 @@
+<template>
+  <table width="100%">
+      <tr>
+       <td style="width:150px">Prometheus Sources:</td>
+       <td style="width:150px" align="left">
+      <el-select v-model="currentPrometheusServerLabel" filterable @change="handlePrometheusSwitch" v-if="currentPrometheusServer">
+ 
+          <el-option v-for="value in prometheusServers" :key="value.namespace+'.'+value.name" :label="value.namespace+'.'+value.name" :value="value.namespace+'.'+value.name"></el-option>
+
+      </el-select>
+      </td>
+      <td style="width:150px">time range select:</td>
+       <td style="width:150px" align="left">
+      <el-select v-model="selectedTimeLabel" filterable @change="handleSelectTime" v-if="currentPrometheusServer">
+ 
+          <el-option v-for="value in times" :key="value.secs" :label="value.label" :value="value.label"></el-option>
+      
+      </el-select>
+      </td>
+      <td style="width:150px">refresh interval:</td>
+       <td style="width:150px" align="left">
+      <el-select v-model="refresh_interval" filterable @change="handleSelectRefreshInterval" v-if="currentPrometheusServer">
+ 
+          <el-option v-for="value in refresh_intervals" :key="value.refresh_interval" :label="value.label" :value="value.refresh_interval"></el-option>
+      
+      </el-select>
+      </td>
+      <td style="width:150px"><el-button type="primary" size="small" @click="loadMetrics">Refresh</el-button></td>
+      <td style="width:(100%-1050px)"></td>
+
+      </tr>
+      <tr>
+       <td colspan="8">
+          <div class="Echarts">
+            <div :id="chartId" style="width: 100%;height: 300px"></div>
+          </div>
+       </td>
+      </tr>
+     </table>
+</template>
+<script>
+/*检查已安装在k8s集群中prometheus的服务端*/
+import {listPrometheuses,getContainerMetricsMemAndFs,getContainerMetricsCpu} from "@/api/crds/monitoring.coreos.com/v1/prometheus"
+/*用户检查prometheus使用的service*/
+import {listServicesWithNs} from "@/api/services"
+import moment from 'moment'
+function arrayDuplicateRemovalSort(arr1,arr2){
+const arr = arr1.concat(arr2)
+return [...new Set( arr.sort( ( a , b )  => a > b ? 1 : -1 ) ) ]
+}
+export default {
+  name: "KoDetailContainerMetrics",
+  props: {
+    cluster: String,
+    containerInfo: Object,
+    yamlInfo: Object,
+  },
+  watch: {
+    cluster:{
+      async handler(newValue,oldValue) {
+        if (newValue!=oldValue) {
+           await this.loadPrometheuses()
+           this.loadMetrics()
+           this.stopTimeTick()
+           this.startTimeTick()
+        }
+      },
+      immediate: true,
+      deep: true,
+    },
+    containerInfo: {
+      async handler(newValue) {
+        if (newValue) {
+           await this.loadPrometheuses()
+           this.loadMetrics()
+           this.stopTimeTick()
+           this.startTimeTick()
+        }
+      },
+      immediate: true,
+      deep: true,
+    },
+  },
+  data() {
+    return {
+      loading: false,
+      //prometheus服务器列表
+      prometheusServers:[],
+      currentPrometheusServer:null,
+      currentPrometheusServerLabel:null,
+      //定时
+      intervalId:null,
+      chartId: this.cluster+"|"+this.yamlInfo.metadata.namespace +"|"+this.yamlInfo.metadata.name+"|"+ this.containerInfo.name,
+      myChart:null,
+      //时间跨度选择
+      secs: 1800,/*查询时间长度*/
+      time_interval: 1,/*坐标间隔*/
+      times:[
+        {
+          label:"Half Hour ago",
+          secs:1800,/*半小时*/
+          time_interval: 1
+        },
+        {
+          label:"1 Hour ago",
+          secs:3600,/*1小时*/
+          time_interval: 2
+        },
+        {
+          label:"12 Hour ago",
+          secs:3600*12,/*12小时*/
+          time_interval: 24
+        },
+        {
+          label:"24 Hour ago",
+          secs:3600*24,/*24小时*/
+          time_interval: 48
+        }
+      ],
+      selectedTimeLabel:"Half Hour ago",
+      //刷新频率
+      refresh_interval: 60000,/*一分钟*/
+      refresh_intervals:[
+        {
+          refresh_interval: -1,/*不自动刷新*/
+          label:"no auto refresh"
+        },
+        {
+          refresh_interval: 60000,/*1分钟*/
+          label:"1 Minutes"
+        },
+        {
+          refresh_interval: 300000,/*5分钟*/
+          label:"5 Minutes"
+        },
+        {
+          refresh_interval: 600000,/*10分钟*/
+          label:"10 Minutes"
+        }
+      ]
+   }
+  },
+  async mounted() {
+    await this.loadPrometheuses()
+  },
+  methods: {
+    //加载Prometheus服务器列表
+    async loadPrometheuses(){
+      let prometheusServers=[]
+      const prometheusServers_res=await  listPrometheuses(this.cluster)
+      for(let i=0,s=prometheusServers_res.items.length;i<s;i++){
+         const prometheusServerItem=prometheusServers_res.items[i]
+         const name=prometheusServerItem.metadata.name
+         const namespace=prometheusServerItem.metadata.namespace
+         const prefix=prometheusServerItem.spec.routePrefix
+
+         const services_res=await listServicesWithNs(this.cluster,namespace)
+         const service=services_res.items.find(item=>item.spec.selector["prometheus"]==name)
+         if(service){
+           prometheusServers.push({
+              name:name,
+              namespace:namespace,
+              service:service.metadata.name,
+              prefix:prefix
+           })
+         }
+
+      }
+      this.prometheusServers=prometheusServers
+      this.currentPrometheusServer=prometheusServers[0]
+      this.currentPrometheusServerLabel=prometheusServers[0].namespace+"."+prometheusServers[0].name
+    },
+    //切换Prometheus服务器
+    handlePrometheusSwitch(value){
+      this.currentPrometheusServer=this.prometheusServers.find(item=>value==item.namespace+"."+item.name)
+      if(this.currentPrometheusServer){
+       this.stopTimeTick()
+       this.loadMetrics()
+       this.startTimeTick()
+      }
+    },
+    //选择查询时间范围和坐标间隔
+    handleSelectTime(value){
+      this.selectedTime=this.times.find(item=> value==item.label)
+      this.secs=this.selectedTime.secs
+      this.time_interval=this.selectedTime.time_interval;
+      if(this.currentPrometheusServer && this.selectedTime){
+       
+
+       this.stopTimeTick()
+       this.loadMetrics()
+       this.startTimeTick()
+      }
+    },
+    //选择刷新频率
+    handleSelectRefreshInterval(value){
+      this.refresh_interval=value
+      if(this.currentPrometheusServer && this.selectedTime){
+       
+
+       this.stopTimeTick()
+       this.loadMetrics()
+       this.startTimeTick()
+      }
+    },
+    startTimeTick(){
+      if(this.refresh_interval<0){
+        return
+      }
+      if (this.intervalId != null) {
+        return;
+      }
+      // 计时器为空，操作
+      this.intervalId = setInterval(() => {
+         //console.log("读取指标" + new Date());
+         this.loadMetrics()
+      }, this.refresh_interval);
+    },
+    stopTimeTick(){
+      if (!this.intervalId) {
+        return;
+      }
+      clearInterval(this.intervalId); //清除计时器
+      this.intervalId = null; //设置为null
+    },
+    loadMetrics(){
+      if(this .myChart!=null){
+        this .myChart.showLoading({
+          text: 'loading',
+          color: '#c23531',
+          textColor: '#000',
+          maskColor: 'black',
+          zlevel: 0,
+        })
+      }
+      
+    Promise.all([getContainerMetricsMemAndFs(
+        this.cluster,
+        this.currentPrometheusServer.namespace,
+        this.currentPrometheusServer.service,
+        this.currentPrometheusServer.prefix,
+        this.yamlInfo.metadata.namespace,
+        this.yamlInfo.metadata.name,
+        this.containerInfo.name,
+        this.secs /*半小时*/
+      ),getContainerMetricsCpu(
+        this.cluster,
+        this.currentPrometheusServer.namespace,
+        this.currentPrometheusServer.service,
+        this.currentPrometheusServer.prefix,
+        this.yamlInfo.metadata.namespace,
+        this.yamlInfo.metadata.name,
+        this.containerInfo.name,
+        this.secs /*半小时*/
+      )]).then(res=>{
+        if(this .myChart!=null){
+           this .myChart.hideLoading()
+        }
+        this.initCharts(res)
+      })
+    },
+    initCharts(res) {
+      const echarts = require('echarts');
+      if(this .myChart==null)
+      this .myChart = echarts.init(document.getElementById(this.chartId));
+      
+
+      // 设置图表的配置项
+      let option = {
+           tooltip: {
+              trigger: 'axis', // 显示横坐标值
+           },
+           legend: {
+            data: ['cpu','fs','mem'],
+            textStyle: {
+              color: '#FFF',
+            },
+           },
+           xAxis: {
+             type: 'category',
+             boundaryGap: false,
+             //min是半小时前，max是当前时间
+             data: arrayDuplicateRemovalSort(res[0].data.result[0].values.map(items=>{
+               return   moment(items[0]*1000).format('HH:mm')
+             }),res[1].data.result.length>0?res[1].data.result[0].values.map(items=>{
+               return   moment(items[0]*1000).format('HH:mm')
+             }):[]),
+             axisLabel: {
+               interval:this.time_interval, // 坐标轴刻度间隔
+             }
+           },
+           yAxis: [{
+             type: 'value',
+             splitLine: {
+               show: false, //去除网格线
+             },
+             axisLine: {
+            //y轴线的颜色以及宽度
+              show: true,
+              lineStyle: {
+                color: '#FFFFFF',
+                width: 1,
+                type: 'solid',
+              },
+             },
+          
+             splitLine: {
+              show: true,
+              lineStyle: {
+                 color: 'rgba(255,255,255,0.3)',
+              },
+             },
+             name: 'c', //单位
+             nameLocation: 'end', // (单位个也就是在在Y轴的最顶部)
+           },
+           {
+             type: 'value',
+             splitLine: {
+               show: false, //去除网格线
+             },
+             axisLine: {
+            //y轴线的颜色以及宽度
+              show: true,
+              lineStyle: {
+                color: '#FFFFFF',
+                width: 1,
+                type: 'solid',
+              },
+             },
+          
+             splitLine: {
+              show: true,
+              lineStyle: {
+                 color: 'rgba(255,255,255,0.3)',
+              },
+             },
+             name: 'MiB', //单位
+             nameLocation: 'end', // (单位个也就是在在Y轴的最顶部)
+          
+           },
+           {
+             type: 'value',
+             splitLine: {
+               show: false, //去除网格线
+             },
+             axisLine: {
+            //y轴线的颜色以及宽度
+              show: true,
+              lineStyle: {
+                color: '#FFFFFF',
+                width: 1,
+                type: 'solid',
+              },
+             },
+          
+             splitLine: {
+              show: true,
+              lineStyle: {
+                 color: 'rgba(255,255,255,0.3)',
+              },
+             },
+             name: 'MiB', //单位
+             nameLocation: 'end', // (单位个也就是在在Y轴的最顶部)
+          
+           }
+          ],
+           series: [
+           {
+                name: "cpu",
+                data: res[1].data.result.length>0 ? res[1].data.result[0].values.map(items=>Number(items[1])):[],
+                type: 'line',
+                tooltip: {
+                    valueFormatter: function (value) {
+                        return value + ' c';
+                    }
+                }
+                
+           },
+           {
+                name: "fs",
+                data: res[0].data.result[0].values.map(items=>Number(items[1])/1024.00/1024.00),
+                type: 'line',
+                tooltip: {
+                    valueFormatter: function (value) {
+                        return value + ' MiB';
+                    }
+                }
+           },
+           {
+                name: "mem",
+                data: res[0].data.result[1].values.map(items=>Number(items[1])/1024.00/1024.00),
+                type: 'line',
+                tooltip: {
+                    valueFormatter: function (value) {
+                        return value + ' MiB';
+                    }
+                }
+           }
+           ]
+      };
+      this .myChart.setOption(option);
+    }
+  },
+  destroyed(){
+    // 在页面销毁后，清除计时器
+    this.stopTimeTick();
+  }
+}
+</script>
